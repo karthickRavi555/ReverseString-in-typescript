27cc7ccc3a7eaf311e06600817301860741fc500
karthickRavi555 <30966674+karthickRavi555@users.noreply.github.com>
2017-11-02 10:52:48 +0530

    Add files via upload

---
+++ ex1.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ex1',
+  templateUrl: './ex1.component.html',
+  styleUrls: ['./ex1.component.css']
+})
+export class Ex1Component implements OnInit {
+ x = "Reverse program";
+  
+ 
+  // "ynoT"
+  constructor() { }
+
+  
+
+  ngOnInit() {
+   this.x = this.x.toString().split("").reverse().join("");
+    
+    
+  }
+
+}
